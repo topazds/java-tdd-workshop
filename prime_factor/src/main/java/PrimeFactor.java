4c3293117b5b84832621a8167a4b004c6109b4fe
Padungsak Kloynimnuan <padungsak.k@kasikornbank.com>
2017-07-20 17:41:59 +0700

    get solution3 & change algorithm

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -4,8 +4,8 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> result = new ArrayList<Integer>();
-        if(number ==2){
-            result.add(2);
+        if(number!=1){
+            result.add(number);
         }
         return result;
     }
--- prime_factor/src/test/java/PrimeFactorTest.java
+++ prime_factor/src/test/java/PrimeFactorTest.java
@@ -20,7 +20,7 @@ public class PrimeFactorTest {
         assertEquals(list(2), primeFactor.of(2));
     }
 
-    @Test @Ignore
+    @Test
     public void should_return_3_when_number_is_3() {
         PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(list(3), primeFactor.of(3));
